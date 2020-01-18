8be3ccf5089a74393e6d06cd636b2dce07516017
Ozayr Moorad <ozayrmoorad@gmail.com>
2020-01-18 15:28:23 +0200

    added croper function

--- croper/croper.py
+++ croper/croper.py
@@ -27,6 +27,7 @@ state = list()
 
 
 def select_region_elastic(image, sub_region_select=False):
+    # convert image to RGB so that the selection boxes can show up if image is gray scale
     if len(image.shape) == 2:
         image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 
@@ -36,16 +37,26 @@ def select_region_elastic(image, sub_region_select=False):
     sub_region_circles = []
     sub_region_squares = []
 
+    # ==========================================================================
+    # create inatruction bar to show instruction info
     instruction_image = np.zeros((30, image.shape[1], 3), np.uint8)
     text = '[r]-reset [q]-quit'
     instruction_image = put_text_on_image(instruction_image, image.shape[1], text)
 
+    # ==========================================================================
+
+    # ============================================================================================================
+    # MAIN REGION CROP
+    # while loop that allows selection of a region in an image
     def select_main_region_callback(event, x, y, flags, param):
+        # callback that will outline the region selected on the image
+        # nb when done the last and first selection point will be joined to form a closed region
         if event == cv2.EVENT_LBUTTONDOWN:
             cv2.circle(buffer_image, (x, y), 2, (0, 0, 255), 2)
             main_region_contour.append([[x, y]])
             cv2.drawContours(buffer_image, np.array([main_region_contour[-2:]]), -1, (0, 0, 255), 1)
 
+    # create buffer image to reset
     buffer_image = image.copy()
 
     while 1 and not sub_region_select:
@@ -70,11 +81,18 @@ def select_region_elastic(image, sub_region_select=False):
             cv2.destroyAllWindows()
             return 0
 
+    # create mask to mask out the selected region from original image
     mask = np.zeros(image.shape, np.uint8)
     if main_region_contour:
         cv2.drawContours(mask, np.array([main_region_contour]), -1, (255, 255, 255), -1)
         buffer_image = cv2.bitwise_and(image, mask)
 
+    # =========================================================================================================
+
+    # ==========================================================================================================
+    # SUB REGION CROP
+    # =====================================================================
+    # call backs for each shape of the sub region select
     def select_sub_region_callback_elastic(event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
             cv2.circle(buffer_image_2, (x, y), 2, (255, 0, 0), 2)
@@ -117,8 +135,10 @@ def select_region_elastic(image, sub_region_select=False):
             np.copyto(buffer_image_2, state)
             cv2.rectangle(buffer_image_2, temp[0], (x, y), (0, 255, 0), 2)
 
+    # ===========================================================================
+    # buffer image for reset
     buffer_image_2 = buffer_image.copy()
-
+    # dict of functions
     callbacks = {0: select_sub_region_callback_elastic, 1: select_sub_region_callback_circle,
                  2: select_sub_region_callback_square}
     callback_names = {0: 'elastic crop', 1: 'circle crop',
@@ -166,19 +186,22 @@ def select_region_elastic(image, sub_region_select=False):
             return -1
 
     mask = np.zeros(image.shape, np.uint8)
+    #  if contours in elastic crop then crop
     if sub_region_contours:
         for contour_ in sub_region_contours:
             cv2.drawContours(mask, np.array([contour_]), -1, (255, 255, 255), -1)
-
+    # if circles them crop
     if sub_region_circles:
         for circle in sub_region_circles:
             cv2.circle(mask, circle[0], circle[1], (255, 255, 255), -1)
-
+    # if squares then crop
     if sub_region_squares:
         for square in sub_region_squares:
             cv2.rectangle(mask, square[0], square[1], (255, 255, 255), -1)
 
     buffer_image = cv2.bitwise_and(buffer_image, cv2.bitwise_not(mask))
+    # =============================================================================================================================
+
     image = cv2.cvtColor(buffer_image, cv2.COLOR_BGR2GRAY)
 
     if not sub_region_select:
@@ -189,8 +212,6 @@ def select_region_elastic(image, sub_region_select=False):
         right = positions[1].max()
         image = image[top:bottom, left:right]
 
-    show(image, 'Final Selected Region')
-
     return image
 
 
@@ -199,4 +220,5 @@ if __name__ == '__main__':
     root.withdraw()
     root.filename = filedialog.askopenfilename(title="Select file",
                                                filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
-    select_region_elastic(cv2.imread(root.filename))
+    image = select_region_elastic(cv2.imread(root.filename))
+    show(image, 'Final Selected Region')
